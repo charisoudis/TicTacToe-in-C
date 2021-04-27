f225f0e5f339534693a4d267500abcb989547082
achariso <thanasis.harisoudis@gmail.com>
2021-04-27 21:06:32 +0300

    create CLion project
    Signed-off-by: achariso <thanasis.harisoudis@gmail.com>

---
+++ compatibility_utils.c
@@ -0,0 +1,25 @@
+#include <sys/ioctl.h>
+#include "compatibility_utils.h"
+
+void enable_raw_mode()
+{
+    struct termios term;
+    tcgetattr(0, &term);
+    term.c_lflag &= ~(ICANON | ECHO); // Disable echo as well
+    tcsetattr(0, TCSANOW, &term);
+}
+
+void disable_raw_mode()
+{
+    struct termios term;
+    tcgetattr(0, &term);
+    term.c_lflag |= ICANON | ECHO;
+    tcsetattr(0, TCSANOW, &term);
+}
+
+bool kbhit()
+{
+    int bytes_waiting;
+    ioctl(0, FIONREAD, &bytes_waiting);
+    return bytes_waiting > 0;
+}
